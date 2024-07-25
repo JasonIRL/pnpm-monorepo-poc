241776613c09eadf910f7b202d7846fbe5d3a2c3
Jason Gunning <JasonIRL@users.noreply.github.com>
2024-07-25 15:44:14 -0500

    checking git status

--- packages/migration-cli/index.js
+++ packages/migration-cli/index.js
@@ -3,6 +3,7 @@
 const { runMigration, migrate } = require("code-migrate");
 const { program } = require("commander");
 const chalk = require("chalk");
+const { simpleGit, CleanOptions } = require("simple-git");
 
 program.option("-v, --version", "output the version number");
 program.option("-l, --list", "list migrations that can be run");
@@ -17,6 +18,14 @@ program.parse();
 
 const options = program.opts();
 
+const git = simpleGit();
+
+git.status(undefined, (err, data) => {
+  if (data.isClean()) {
+    console.log("clean!");
+  }
+});
+
 if (options.version) {
   console.log(`alloy migration util v${require("./package.json").version}`);
 }
@@ -25,22 +34,36 @@ if (options.all) {
   runMigration({
     cwd: process.cwd(),
     migrationFilePath: require.resolve("./migrations/_all.js"),
+    reportFile: "migrate-report.md",
   });
 }
 
 if (options.migrate) {
-  if (options.migrate === "001") {
-    runMigration({
-      cwd: process.cwd(),
-      migrationFilePath: require.resolve("./migrations/001-gitignore.js"),
+  git
+    .status(undefined, (err, data) => {})
+    .then((repoStatus) => {
+      const isClean = repoStatus.isClean();
+
+      if (!isClean) {
+        console.log(
+          `${chalk.redBright.bold("Please commit your changes before running migrations")}`
+        );
+        process.exit(1);
+      }
+
+      if (options.migrate === "001") {
+        runMigration({
+          cwd: process.cwd(),
+          migrationFilePath: require.resolve("./migrations/001-gitignore.js"),
+        });
+      }
+      if (options.migrate === "002") {
+        runMigration({
+          cwd: process.cwd(),
+          migrationFilePath: require.resolve("./002.js"),
+        });
+      }
     });
-  }
-  if (options.migrate === "002") {
-    runMigration({
-      cwd: process.cwd(),
-      migrationFilePath: require.resolve("./002.js"),
-    });
-  }
 }
 
 if (options.list || options.listAll) {
