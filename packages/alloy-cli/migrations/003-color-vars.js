cbc0da332366cb1c09e362c238bbbe89fca94b2a
Jason Gunning <JasonIRL@users.noreply.github.com>
2024-07-29 12:27:33 -0500

    token migration POC

--- packages/alloy-cli/migrations/003-color-vars.js
+++ packages/alloy-cli/migrations/003-color-vars.js
@@ -1,25 +1,79 @@
 const getMeta = require('../utils/get-meta');
 const logger = require('../utils/logger');
-const keyCheck = require('../utils/key-check');
-const prettier = require('prettier');
 
 const DEFINITION = getMeta('003');
 
+const mappings = require('./003-color-var-mappings.json');
+
 migrate(DEFINITION.key, ({ transform, after }) => {
-  let run = false;
+  const issues = {
+    issue:
+      'The following semantic slots were not found in the mappings file, and will need to be manually updated.',
+    unmatchedTokens: [],
+  };
+
   transform(
     `${DEFINITION.key}: ${DEFINITION.description}`,
-    '**/*.js',
+    '**/*.scss',
     ({ source, fileName }) => {
-      // await prettier.format(source, { parser: "babel" });
-      run = true;
-      //   console.log({ files: `${process.cwd()}/${fileName}` });
+      const { tokenized, unmatchedTokens } = swapSlotForToken(source, fileName);
+
+      if (unmatchedTokens?.length) {
+        issues.unmatchedTokens = [
+          ...issues.unmatchedTokens,
+          ...unmatchedTokens,
+        ];
+      }
+
+      if (tokenized !== source) {
+        const noPrefix = removeGetSemanticSlotPrefixes(tokenized);
+        const replaced = swapGetSemanticSlotForVariables(noPrefix);
+
+        return replaced;
+      }
+
       return source;
     },
   );
-  after(() => {
-    if (run) {
-      logger(DEFINITION.key);
-    }
-  });
+  after(() => logger(DEFINITION.key, issues));
 });
+
+/**
+ *
+ * @param {string} source
+ * @returns {{tokenized: string, unmatchedTokens: string[]}}
+ */
+function swapSlotForToken(source, fileName) {
+  // Regex to find the contents within a get-semantic-slot call
+  const matches = source.match(/(?<=get-semantic-slot\()([\S\s])*?(?=\))/g);
+
+  if (!matches) return source;
+
+  let tokenized = source;
+  const unmatchedTokens = [];
+
+  for (const match of matches) {
+    // get-semantic-slot can take a theme-name param, we don't need it, so it can be ignored
+    const split = match.trim().split(',');
+    const lookup = split[0].replace(/["']/g, '');
+
+    // Find the mapped token
+    const replacement = mappings.find((item) => item.slot === lookup);
+
+    if (replacement) {
+      tokenized = tokenized.replace(match, replacement.token);
+    } else {
+      unmatchedTokens.push({ semanticSlotName: lookup, fileName });
+    }
+  }
+
+  return { tokenized, unmatchedTokens };
+}
+
+function removeGetSemanticSlotPrefixes(input) {
+  return input.replace(/[\w'-.]+(?=\get-semantic-slot)/g, '');
+}
+
+function swapGetSemanticSlotForVariables(input) {
+  return input.replace(/get-semantic-slot\(/g, 'var(');
+}
--- packages/alloy-cli/utils/logger.js
+++ packages/alloy-cli/utils/logger.js
@@ -2,12 +2,13 @@ const { writeFileSync, existsSync } = require('node:fs');
 
 const LOGFILE = `${process.cwd()}/migration-log.json`;
 
-function logger(key) {
+function logger(key, issues) {
   const log = existsSync(LOGFILE) ? require(LOGFILE) : [];
 
   log.push({
     key,
     runDate: new Date().toISOString(),
+    issues,
   });
 
   writeFileSync(LOGFILE, JSON.stringify(log, null, 2));
